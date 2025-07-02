a2976a8199c15a642f3ffb7150a8e92ee6076a76
shravan kumar <shravan95741998@gmail.com>
2025-07-02 19:18:23 +0530

    companies

--- src/stores/pages/mobilePage.jsx
+++ src/stores/pages/mobilePage.jsx
@@ -1,78 +1,62 @@
-
-import React, { useState } from 'react'
-import { mobileData } from '../data/mobiles'
-import Navbar from '../components/Navbar'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { mobileData } from '../data/mobiles';
+import Navbar from '../components/Navbar';
+import { Link } from 'react-router-dom';
 
 const MobilePage = () => {
+	const [selectedProduct, setSelectedProduct] = useState([]);
 
-	const [selectedProduct, setSelectedProduct] = useState([])
+	const companies = [...new Set(mobileData.map(item => item.company))];
 
-	const companyHandler = (mango) => {
-		if (selectedProduct.includes(mango)) {
-			setSelectedProduct(selectedProduct.filter(item => item !== mango))
+	const companyHandler = (company) => {
+		if (selectedProduct.includes(company)) {
+			setSelectedProduct(selectedProduct.filter(item => item !== company));
 		} else {
-			setSelectedProduct([mango])
+			setSelectedProduct([...selectedProduct, company]);
 		}
-	}
+	};
 
-
-	const filteredProduct = selectedProduct.length === 0 ?
-		mobileData : mobileData.filter((orange) => selectedProduct.includes(orange.company))
-
+	const filteredProduct =
+		selectedProduct.length === 0
+			? mobileData
+			: mobileData.filter((item) => selectedProduct.includes(item.company));
 
 	return (
 		<>
 			<Navbar />
 			<div className="fullpage">
-				<div className='pro-selected'>
-
-					{mobileData.map((phone) => {
-						return (
-							<div className='pro-input'>
-								<label>
-									<input type='checkbox'
-										checked={selectedProduct.includes(phone.company)}
-										onChange={() => companyHandler(phone.company)}
-
-									/>
-									{phone.company}
-								</label>
-							</div>
-
-						)
-
-					})}
+				<div className="pro-selected">
+					{companies.map((company) => (
+						<div key={company} className="pro-input">
+							<label>
+								<input
+									type="checkbox"
+									checked={selectedProduct.includes(company)}
+									onChange={() => companyHandler(company)}
+								/>
+								{company}
+							</label>
+						</div>
+					))}
 				</div>
-				
-				
 
-
-				<div className='pageSection'>
-					{filteredProduct.map((item) => {
-						return (
-							<div>
-
-								<Link to={`/mobiles/${item.id}`}>
-									<div className='pageImg'>
-										<img src={item.image} alt='' />
-									</div>
-								</Link>
-
-								<div className='proModel'>
-									Company: {item.company}, Price: {item.price}
+				<div className="pageSection">
+					{filteredProduct.map((item) => (
+						<div key={item.id}>
+							<Link to={`/mobiles/${item.id}`}>
+								<div className="pageImg">
+									<img src={item.image} alt="" />
 								</div>
+							</Link>
+							<div className="proModel">
+								Company: {item.company}, Price: {item.price}
 							</div>
-						)
-
-					})}
+						</div>
+					))}
 				</div>
-
-
 			</div>
-
 		</>
-	)
-}
+	);
+};
 
-export default MobilePage
+export default MobilePage;
